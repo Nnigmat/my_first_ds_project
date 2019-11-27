0c29ba204ac49c1563c3599d8dc4b10ec42b2641
s.dinmuhametov <perfog@mail.ru>
2019-11-27 20:31:17 +0300

    items_in_folder fix

--- nameserver/db.py
+++ nameserver/db.py
@@ -3,6 +3,8 @@ import os
 
 class Files():
     def __init__(self, name):
+        #name  = name of database, 
+
         self.db_name = 'f'
         self.name = name
         self.db = pickledb.load(name, os.path.isfile("./"+name))
@@ -10,26 +12,35 @@ class Files():
         self.db.dump()
 
     def is_file_exist(self, name):
+        #filename
         res = self.db.lgetall(self.db_name)
         return name in res
 
     def add(self, name):
+        #filename
         self.db.ladd(self.db_name, name)
         self.db.dump()
 
     def del_file(self, name):
+        #filename
         files = self.db.lgetall(self.db_name)
         self.db.lpop(self.db_name, files.index(name))
         self.db.dump()
 
     def items_in_folder(self, name):
+        #filename
         files = self.db.lgetall(self.db_name)
         items = []
+        folders = []
         for i in range(len(files)):
             if files[i].startswith(name):
-                items.append(files[i])
-        return items
+                if files[i][-1] == '/':
+                    if files[i] != name:
+                        folders.append(files[i])
+                else:
+                    items.append(files[i])
+        return (folders, items) 
 
-    def get_all_files(self):
+    def get_all(self):
         return self.db.lgetall(self.db_name)
 
