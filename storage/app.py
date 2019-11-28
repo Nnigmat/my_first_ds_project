5d24625c37bf95e303935b85e27140b424588721
Apostrov <amir.subaev@gmail.com>
2019-11-28 17:07:42 +0300

    Storage add heartbeat

--- storage/app.py
+++ storage/app.py
@@ -1,13 +1,17 @@
 import sys
 import os
+import threading
+import requests
 from flask import Flask, request, send_file, abort
 
 app = Flask(__name__)
 ROOTDIR = 'FILE/'
+NODES = ['localhost:8080', 'localhost:8081']
+FAILED_NODES = []
 app.config['UPLOAD_FOLDER'] = ROOTDIR
 
 
-@app.route('/<path:path>', methods=['GET', 'POST'])
+@app.route('/file/<path:path>', methods=['GET', 'POST'])
 def upload_file(path):
     filepath = os.path.join(ROOTDIR, path)
     if request.method == 'GET':
@@ -22,10 +26,34 @@ def upload_file(path):
         return filepath, 201
 
 
+def heartbeat_ask():
+    timer = threading.Timer(10.0, heartbeat_ask)
+    timer.start()
+    for node in NODES:
+        url = "http://" + node + '/'
+        try:
+            r = requests.get(url=url)
+        except requests.exceptions.ConnectionError:
+            if node in FAILED_NODES:
+                NODES.remove(node)
+                FAILED_NODES.remove(node)
+                print(node, "removed")
+            else:
+                FAILED_NODES.append(node)
+                print(node, "failed")
+        else:
+            if node in FAILED_NODES:
+                FAILED_NODES.remove(node)
+    return timer
+
+
 if __name__ == '__main__':
     if sys.argv[1:]:
         port = int(sys.argv[1])
     else:
         port = 8080
 
-    app.run(debug=True, port=port)
+    heartbeat = heartbeat_ask()
+
+    app.run(port=port)
+    heartbeat.cancel()
