a5f4e11a80839d7f60918b12a0dd0eb23a5b7518
Nikita <nnigmat@gmail.com>
2019-12-02 17:40:51 +0300

    Fix

--- nameserver/app.py
+++ nameserver/app.py
@@ -50,14 +50,12 @@ def dirs(path):
         if request.form['method'] == 'PUT':
             if not files.exists(d_path):
                 files.add(d_path)
-                # send created folder to server
                 node_man.add_dir(d_path)
             else:
                 flash('Directory already exists')
         elif request.form['method'] == 'DELETE':
             if files.exists(d_path):
                 files.del_file(d_path)
-                # send deleted folder to server
                 node_man.del_dir(d_path)
             else:
                 flash("Directory doesn't exists")
@@ -88,9 +86,15 @@ def file(path):
         elif request.form['method'] == 'CREATE':
             if not files.exists(f_path):
                 files.add(f_path)
+            else:
+                flash('File already exists')
         elif request.form['method'] == 'DELETE':
             if files.exists(f_path):
                 files.del_file(f_path)
+            else:
+                flash('File does not exist')
+        else:
+            flash('Something went wrong')
     return redirect(url_for('dirs'))
 
 
@@ -131,9 +135,11 @@ def move():
         if not target.endswith('/'):
             target = target + '/'
 
+        # Move file in both nameserver and nodes
         files.move_file(source, target, path)
-
         node_man.move_dir(source, target)
+
+        # Redirect
         if path == '/':
             return redirect('/dirs/')
         else:
